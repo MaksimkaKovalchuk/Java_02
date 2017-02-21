42796b3c66f8b15c6d037e00ccb7320ea489c42b
Maksim <maksimkakovalchuk1203@gmail.com>
2017-02-21 19:34:51 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Максим on 21.02.2017.
+ */
+public class Main {
+    //TODO ADD a new method
+    public static void main (String[] args) {
+        System.out.println("Hello World! lab2");
+    }
+}
